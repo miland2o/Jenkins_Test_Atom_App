1169784bce122b70940e3cff3645fd1950c6adda
Milan <milan@svrljig.net>
2020-05-13 01:59:50 +0200

    Initial commit

---
+++ src/test/java/cloud/deuterium/atom/AtomApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.deuterium.atom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
